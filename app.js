e8e0407e98e32805cea56a9d07554c0a2a32de46
AztarTheShadow <adam.parslow@hotmail.com>
2016-09-19 19:33:27 +1000

    Built a basic server

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var express = require ('express');
 var app = express ();
 
 app.set ('port', process.env.PORT, 8080);
+
+app.get ('/', function (req, res) {
+   res.send (__dirname + 'index.html');
+});
+
+app.listen ('port', function () {
+   console.log ("We are listening");
+});
